f5592361fce4f8c0abfd47106b0935bcf3bb4b0b
Katie Fugate <katiiierochelle@yahoo.com>
2023-05-17 16:52:09 -0700

    home page start

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
     return (
-        <div>
-            <h1>HOME PAGE</h1>
+        <div className={'text-center'}>
+            <h1>Welcome to Sustainable Cleaning Solutions</h1>
+            <p>For any questions regarding services before booking, please don't hesitate to contact us directly.</p>
+            <p>Please also refer to our FAQs page for commonly asked questions.</p>
+            <div>
+                <h1>A bit about us</h1>
+                <h4>Our Background</h4>
+                <p>Since our founding, we've worked with numerous clients throughout the area. Great service begins and ends
+                with experienced and friendly professionals, which is why we put so much consideration into selecting
+                only the best to join our team. We complete projects efficiently and on schedule, and go above and
+                beyond to form lasting relationships with our clients.</p>
+            </div>
         </div>
     );
 }
