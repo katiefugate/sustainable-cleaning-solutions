fbf5037a7b11a804f4069aa86170d3f0548b6209
Katie Fugate <katiiierochelle@yahoo.com>
2023-06-13 19:29:49 -0700

    home page changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,24 @@
+import {Box, Heading } from "@chakra-ui/react";
+
 export default function Home() {
     return (
-        <div className=" home-page-container">
-            <h1 className="text-center">Welcome to Sustainable Cleaning Solutions</h1>
-            <div>
+        <div className="home-page-container">
+            <Heading as='h3' size='lg' className="text-center">WELCOME TO SUSTAINABLE CLEANING SOLUTIONS</Heading>
+            <div className='text-center'>
                 <p>Since our founding, we've worked with numerous clients throughout the area. Great service begins and ends
                     with experienced and friendly professionals, which is why we put so much consideration into selecting
                     only the best to join our team. We complete projects efficiently and on schedule, and go above and
                     beyond to form lasting relationships with our clients.</p>
             </div>
             <div className="home-contact-container">
-                <div>
-                    <p>For any questions regarding services before booking, please don't hesitate to contact us directly.</p>
-                    <p>Please also refer to our FAQs page for commonly asked questions.</p>
-                </div>
-                <div>
+                <Box bg="lightgrey" p='6' borderWidth='1px' borderRadius='lg' height='175px'>
                     <h1>Get in touch</h1>
                     <p>Email us: sustainablecleaningsolutions@gmail.com</p>
                     <p>Call or Text at: </p>
-                </div>
+                </Box>
+                <Box bg={require("../images/cleaning.png")} p='6' borderWidth='1px' borderRadius='lg' ml='6' height='175px'>
+                    <img src={require("../images/SustainableCleaningSolutions.png")} width='60%'/>
+                </Box>
             </div>
         </div>
     );
